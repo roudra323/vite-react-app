474c8a1966fd0cceea063af56b18c609e8077149
roudra323 <asirroudra@gmail.com>
2023-03-12 13:06:30 +0600

    colored the buttets

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,16 @@ function App() {
         </nav>
       </div>
       <div className="Main">
-        <h1>Main component</h1>
-        <ul>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-          <li>Is maintained by Facebook</li>
-          <li>Has well over 100K stars on GitHub</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Was first released in 2013</li>
-        </ul>
+        <main>
+          <h1 className="main--title">Fun facts about React</h1>
+          <ul className="main--facts">
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
+        </main>
       </div>
       <div className="footer">
         <footer>
